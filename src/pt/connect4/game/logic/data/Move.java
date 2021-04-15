60e9f4e671b8425aecf7583ef1dc83a2ef4aad42
Nuno Rocha <intersect.nerd@gmail.com>
2021-04-16 00:36:25 +0100

    populated Move.java winth some code

--- src/pt/connect4/game/logic/data/Move.java
+++ src/pt/connect4/game/logic/data/Move.java
@@ -1,4 +1,45 @@
 package pt.connect4.game.logic.data;
 
 public class Move {
+
+    private int row;
+    private int column;
+    private int value;
+
+    public Move(int value) {
+        this.value = value;
+    }
+
+    public Move(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public Move() {
+    }
+
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
